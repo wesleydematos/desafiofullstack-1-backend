8ef8c1e7eb493a066ee72a1ea562f81f4c22e8bf
wesleydematos <wesleydematos3@gmail.com>
2023-03-27 15:07:25 -0300

    feat: create schemas from customers

--- src/schemas/customerSchema.ts
+++ src/schemas/customerSchema.ts
@@ -1,11 +1,19 @@
 import * as yup from "yup";
 import { SchemaOf } from "yup";
-import { ICustomerContactRequest } from "../interfaces";
+import { ICustomerRequest, ICustomerUpdate } from "../interfaces";
 
-export const createCustomerSerializer: SchemaOf<ICustomerContactRequest> = yup
+export const createCustomerSchema: SchemaOf<ICustomerRequest> = yup
   .object()
   .shape({
     name: yup.string().max(50).required(),
     email: yup.string().max(50).required(),
     phoneNumber: yup.string().max(15).required(),
+  });
+
+export const updateCustomerSchema: SchemaOf<ICustomerUpdate> = yup
+  .object()
+  .shape({
+    name: yup.string().max(50).notRequired(),
+    email: yup.string().max(50).notRequired(),
+    phoneNumber: yup.string().max(15).notRequired(),
   });
