6e0e5944d86b56268f9343553e30cd6898d6cc34
wesleydematos <wesleydematos3@gmail.com>
2023-03-30 15:30:50 -0300

    chore: add  cors

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,12 @@ import express from "express";
 import { customerRouters } from "./routes/customers";
 import { contactRouters } from "./routes/contacts";
 import { handleError } from "./errors";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/customers", customerRouters);
 app.use("/contacts", contactRouters);
 
